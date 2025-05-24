a58c319547aba7399acef995b675c8d7e6eecf2d
kaznotaninokaz <kazzzzz0358@gmail.com>
2025-05-24 23:23:58 +0900

    お客様の声完成

---
+++ assets/script/main.js
@@ -0,0 +1,171 @@
+// カルーセル
+$('.voices__slick').slick({
+  dots: true,
+  arrows: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  infinite: false,
+  responsive: [
+    {
+      breakpoint: 980,
+      settings: {
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: '15.3125rem'
+      }
+    },
+    {
+      breakpoint: 950,
+      settings: {
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: '14.6875rem'
+      }
+    },
+    {
+      breakpoint: 925,
+      settings: {
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: '14.0625rem'
+      }
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: '13.4375rem'
+      }
+    },
+    {
+      breakpoint: 875,
+      settings: {
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: '12.8125rem'
+      }
+    },
+    {
+      breakpoint: 850,
+      settings: {
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: '12.1875rem'
+      }
+    },
+    {
+      breakpoint: 825,
+      settings: {
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: '11.5625rem'
+      }
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: '10.9375rem'
+      }
+    },
+    {
+      breakpoint: 750,
+      settings: {
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: '10.3125rem'
+      }
+    },
+    {
+      breakpoint: 725,
+      settings: {
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: '9.6875rem'
+      }
+    },
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: '9.0625rem'
+      }
+    },
+    {
+      breakpoint: 675,
+      settings: {
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: '8.4375rem'
+      }
+    },
+    {
+      breakpoint: 650,
+      settings: {
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: '7.8125rem'
+      }
+    },
+    {
+      breakpoint: 625,
+      settings: {
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: '7.1875rem'
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: '6.5625rem'
+      }
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: '5.9375rem'
+      }
+    },
+    {
+      breakpoint: 550,
+      settings: {
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: '5.3125rem'
+      }
+    },
+    {
+      breakpoint: 500,
+      settings: {
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: '4.0625rem'
+      }
+    },
+    {
+      breakpoint: 450,
+      settings: {
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: '2.5rem'
+      }
+    },
+    {
+      breakpoint: 400,
+      settings: {
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: '2rem'
+      }
+    }
+  ]
+});
+
